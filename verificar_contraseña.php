7602a8ad13b586990a01cb9a5b732676f7724a1e
f7TRY <143319104+f7TRY@users.noreply.github.com>
2023-08-27 12:32:38 +0200

    Update verificar_contraseña.php

--- verificar_contraseña.php
+++ verificar_contraseña.php
@@ -1,15 +1,1 @@
-<?php
-$contraseña_correcta = "agustitogustavo"; // Cambia esto a tu contraseña real
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $password_ingresada = $_POST["password"];
-
-    if ($password_ingresada === $contraseña_correcta) {
-        // Contraseña correcta, redirige a la página protegida
-        header("Location: pagina_protegida.html");
-        exit();
-    } else {
-        echo "Contraseña incorrecta. Intenta de nuevo.";
-    }
-}
-?>
